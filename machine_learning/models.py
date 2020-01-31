d8d58d0d8f3dbe7c4b38b13d97cd73d83a667ba9
Ali Boushehri <ali.shetab.boushehri@gmail.com>
2020-01-31 15:42:50 +0100

    model file

---
+++ machine_learning/models.py
@@ -0,0 +1,91 @@
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ShallowNet(nn.Module):
+    def __init__(self,num_channels ,num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(num_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+
+
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_channels ,num_classes):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(num_channels , 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.embedding = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128, num_classes),
+
+        )
+        
+    def embedding_generator(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.embedding(x)
+        return x
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.embedding(x)
+        x = self.classifier(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+def get_model(model_name, device,num_channels ,num_classes):
+    if model_name == "AlexNet":
+        reshape_size = 256
+        model = AlexNet(num_channels ,num_classes)
+    if model_name == "ShallowNet":
+        reshape_size = 28
+        model = ShallowNet(num_channels ,num_classes)
+    
+    if device is 'cpu':
+        model = model.to(device)
+    return model, reshape_size
