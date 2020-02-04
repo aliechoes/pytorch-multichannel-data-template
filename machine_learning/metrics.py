4c036e397b10de58c298430b010323ad7bd3e7e0
Ali Boushehri <ali.shetab.boushehri@gmail.com>
2020-02-04 03:44:39 +0100

    added cross entropy for the metrics

--- machine_learning/metrics.py
+++ machine_learning/metrics.py
@@ -1,10 +1,21 @@
 import pandas as pd 
 from sklearn.metrics import  accuracy_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import log_loss
+import numpy as np
+
+def custom_log_loss(y_true, y_pred, existing_labels):
+    custom_y_pred = np.zeros((y_true.shape[0], len(existing_labels)))
+    
+    for i in range(0, len(y_true)):
+        j = y_pred[i]
+        custom_y_pred[i,j ] = 1 
+    return log_loss(y_true, custom_y_pred)
 
 
 def metric_history(df, metric_dataframe, epoch, metrics_of_interest ):
     print(("epoch", epoch))
+    existing_labels = df["label"].unique().tolist()
     for s in ["train", "validation", "test"]:
         print(4*"---")
         df_temp = df[ df["set"]==s].reset_index(drop = True).copy()
@@ -23,6 +34,17 @@ def metric_history(df, metric_dataframe, epoch, metrics_of_interest ):
             print("accuracy for the %s set is: %f" % (s, accuracy_score(y_true, y_pred ) ) )
             metric_dataframe = metric_dataframe.append(results_temp, ignore_index=True)
 
+        if "cross_entropy" in metrics_of_interest:
+                results_temp = {
+                    'epoch': epoch+1,
+                    'set':   s,
+                    'metric': 'cross_entropy',
+                    'value': custom_log_loss(y_true, y_pred, existing_labels) 
+                }
+                print("cross_entropy for the %s set is: %f" % (s,  \
+                    custom_log_loss(y_true, y_pred, existing_labels )  ) )
+                metric_dataframe = metric_dataframe.append(results_temp, ignore_index=True)
+  
         
         if "dice" in metrics_of_interest:
             results_temp = {
